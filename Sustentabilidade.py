5936e4f39bc19f49a3f44acff5fce9944b20dde8
ViniciusSete <vnsete7@gmail.com>
2025-04-24 20:34:21 -0300

    Inserção banco de dados

--- Sustentabilidade.py
+++ Sustentabilidade.py
@@ -1,3 +1,53 @@
+'''----------------\\COMEÇO SUSTENTABILIDADE\\----------------'''
+
+digitou_corretamente=False
+while not digitou_corretamente:
+    try:
+        l_agua=float(input("\nQuantos litros de água você consumiu hoje? (Aprox. em litros): "))
+    except ValueError:
+        print("Digitar apenas números!")
+    else:
+        digitou_corretamente=True
+
+        if l_agua <150:
+            agua_susten="Alta sustentabilidade"
+        elif l_agua <= 150 or l_agua <=200:
+            agua_susten="Moderada Sustentabilidade"
+        elif l_agua > 200:
+            agua_susten="Baixa Sustentabilidade"
+
+digitou_corretamente2=False
+while not digitou_corretamente2:
+    try:
+        energia=float(input("\nQuantos kWh de energia elétrica você consumiu hoje?: "))
+    except ValueError:
+        print("Digitar apenas números!")
+    else: 
+        digitou_corretamente2=True
+
+        if energia < 5:
+            energia_susten="Alta Sustentabilidade"
+        elif energia <=5 or energia<=10:
+            energia_susten="Moderada Sustentabilidade"
+        elif energia > 10:
+            energia_susten="Baixa Sustentabilidade"
+            
+digitou_corretamente3=False
+while not digitou_corretamente3:
+    try: 
+        kg_residuos=float(input("\nQuantos kg de resíduos não recicláveis você gerou hoje? Se for em gramas, preencha '0.!!!': "))
+    except ValueError:
+        print("Digitar apenas números válidos!")
+    else:
+        digitou_corretamente3=True
+
+        if kg_residuos <= 0.770: #770 gramas
+            kg_residuos_susten="Alta Sustentabilidade"
+        elif kg_residuos <= 0.770 or kg_residuos <=1.54: 
+            kg_residuos_susten="Moderada Sustentabilidade"
+        elif kg_residuos > 1.54:
+            kg_residuos_susten="Baixa Sustentabilidade"
+
 digitou_corretamente4=False
 while not digitou_corretamente4:
     try:
